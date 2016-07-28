d255e00a265409decc426b6fd524d2fd02642764
Alexander Novikov <novikov@bayesgroup.ru>
2016-07-28 15:16:49 +0300

    Error fixes: now it runs

--- Swapout.lua
+++ Swapout.lua
@@ -13,21 +13,23 @@ function Swapout:__init(p)
    end
    self.noise = {}
    for i = 1,#self.p do
+      -- TODO: move to updateOutput and use input.new()?
       self.noise[i] = torch.Tensor()
    end
    self.gradInput = {}
 end
 
 function Swapout:updateOutput(input)
-   if #self.p ~= #inputs then
+   if #self.p ~= #input then
       error(string.format('<Swapout> unexpected number of inputs %d, expected %d', #inputs, #self.p))
    end
    self.output:resizeAs(input[1])
    if self.train then
       for i = 1,#self.p do
          if self.p[i] > 0 then
+            self.noise[i]:resizeAs(input[i])
             self.noise[i]:bernoulli(1-self.p[i])
-            self.output:add(copy(torch.cmul(input[i], self.noise[i])))
+            self.output:add(torch.cmul(input[i], self.noise[i]))
          else
             self.output:add(input[i])
          end
@@ -44,9 +46,9 @@ function Swapout:updateOutput(input)
 end
 
 function Swapout:updateGradInput(input, gradOutput)
-   self.gradInput:resizeAs(gradOutput):copy(gradOutput)
    if self.train then
       for i = 1,#self.p do
+         self.gradInput[i] = input[i].new()
          self.gradInput[i]:resizeAs(input[i]):copy(gradOutput)
          if self.p[i] > 0 then
             self.gradInput[i]:cmul(self.noise[i])
@@ -54,6 +56,7 @@ function Swapout:updateGradInput(input, gradOutput)
       end
    else
       for i = 1,#self.p do
+         self.gradInput[i] = input[i].new()
          self.gradInput[i]:resizeAs(input[i]):copy(gradOutput)
          if self.p[i] > 0 then
             self.gradInput[i]:mul(1-self.p[i])
